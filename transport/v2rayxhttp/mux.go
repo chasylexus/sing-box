b83c44a7218d17e509901e88f555bc6ae082c8b0
starifly <2003879+starifly@users.noreply.github.com>
2025-12-14 18:01:33 +0800

    Use global Xmux manager lock (xhttp)

--- transport/v2rayxhttp/mux.go
+++ transport/v2rayxhttp/mux.go
@@ -5,7 +5,6 @@ import (
 	"crypto/rand"
 	"math"
 	"math/big"
-	"sync"
 	"sync/atomic"
 	"time"
 
@@ -30,7 +29,6 @@ type XmuxManager struct {
 	connections int32
 	newConnFunc func() XmuxConn
 	xmuxClients []*XmuxClient
-	mtx         sync.Mutex
 }
 
 func NewXmuxManager(options option.V2RayXHTTPXmuxOptions, newConnFunc func() XmuxConn) *XmuxManager {
@@ -63,8 +61,6 @@ func (m *XmuxManager) newXmuxClient() *XmuxClient {
 }
 
 func (m *XmuxManager) GetXmuxClient(ctx context.Context) *XmuxClient {
-	m.mtx.Lock()
-	defer m.mtx.Unlock()
 	for i := 0; i < len(m.xmuxClients); {
 		xmuxClient := m.xmuxClients[i]
 		if xmuxClient.XmuxConn.IsClosed() ||
